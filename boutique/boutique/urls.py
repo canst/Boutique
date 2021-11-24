ea6dc3fdf26ba17feebbdf5284d3f67d94dbd5d2
Yao Sossou <soyames@gmail.com>
2021-11-24 09:19:22 +0000

    Initial commit

---
+++ boutique/boutique/urls.py
@@ -0,0 +1,28 @@
+"""boutique URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import include, url
+from django.contrib import admin
+
+import store.views
+
+from store import views
+
+urlpatterns = [
+    url(r'^$', views.index, name="index"),
+    url(r'^store/', include(('store.urls', 'store'), namespace='store')),
+    url(r'^content/', admin.site.urls),
+]
